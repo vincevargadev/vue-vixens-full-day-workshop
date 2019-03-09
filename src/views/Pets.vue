d0807a27db78dbd91025574376d9f98f3ef7cae8
Vince Varga <vince.varga@smaho.com>
2019-03-09 21:33:43 +0100

    Load dogs into the pets screen

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,13 +1,21 @@
 <template>
   <v-container grid-list-md fluid>
     <v-layout wrap>
-      <v-flex xs12 sm4 md3>
+      <!-- v-for directive to loop over the dogs -->
+      <!-- the individual dogs will be referred to as "dog" -->
+      <v-flex xs12 sm4 md3 v-for="dog in dogs" :key="dog.breed">
         <v-card color="grey lighten-2">
-          <v-img src="https://goo.gl/6CQNDo" height="170px">
-          </v-img>
+          <!-- To bind attributes dynamically we need : "colon" which is a v-bind directive's shortcut -->
+          <v-img :src="dog.img" height="170px"/>
           <v-card-title>
             <div>
-              <h3>Looking for a dog?</h3>
+              <!--
+                Vue uses "mustache" syntax:
+                double curly braces like these: {{ }}.
+                The mustache tag will be replaced with the value of the binded property
+              -->
+              <h3>{{ dog.name }}</h3>
+              <p>{{ dog.breed }}</p>
             </div>
           </v-card-title>
         </v-card>
@@ -15,6 +23,17 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import { dogs } from '../data/dogs'
+
+export default {
+  data() {
+    return { dogs };
+  }
+}
+</script>
+
 
 <style scoped>
 p {
