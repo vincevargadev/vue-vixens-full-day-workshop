05544a20b08c4071a74da1b22376491594867c53
Vince Varga <vince.varga@smaho.com>
2019-03-10 10:32:08 +0100

    Display text when favorites list is empty

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <v-list>
-      <v-subheader>My Favorites</v-subheader>
-      <v-list-tile avatar v-for="(dog, index) in favorites" :key="index" @click="{}">
-        <v-list-tile-avatar>
-          <img :src="dog.img">
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          {{dog.name}}
-        </v-list-tile-content>
-        <v-list-tile-action>
-          <v-icon>delete</v-icon>
-        </v-list-tile-action>
-      </v-list-tile>
+      <v-subheader v-if="!favorites.length">Your favorites list is empty</v-subheader>
+      <template v-else>
+        <v-subheader>My Favorites</v-subheader>
+        <v-list-tile avatar v-for="(dog, index) in favorites" :key="index" @click="{}">
+          <v-list-tile-avatar>
+            <img :src="dog.img">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            {{ dog.name }}
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-icon>delete</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </template>
     </v-list>
   </div>
 </template>
